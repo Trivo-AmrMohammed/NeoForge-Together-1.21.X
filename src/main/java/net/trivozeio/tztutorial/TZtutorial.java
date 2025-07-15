d2eb96a6c05a58efc7c5315c7c42f2a100f9dec0
ZeioDust <aboodhassanxx999@gmail.com>
2025-07-15 11:17:04 +0300

    Zeio Osmium New Item

--- src/main/java/net/trivozeio/tztutorial/TZtutorial.java
+++ src/main/java/net/trivozeio/tztutorial/TZtutorial.java
@@ -1,5 +1,6 @@
 package net.trivozeio.tztutorial;
 
+import net.trivozeio.tztutorial.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -41,6 +42,7 @@ public class TZtutorial {
     public static final Logger LOGGER = LogUtils.getLogger();
 
 
+
     public TZtutorial(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -49,6 +51,8 @@ public class TZtutorial {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -63,7 +67,10 @@ public class TZtutorial {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.OSMIUM);
+            event.accept(ModItems.RAW_OSMIUM);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/trivozeio/tztutorial/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.trivozeio.tztutorial.item;
+
+import net.trivozeio.tztutorial.TZtutorial;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TZtutorial.MODID);
+
+    public static final DeferredItem<Item> OSMIUM = ITEMS.register("osmium",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_OSMIUM = ITEMS.register("raw_osmium",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
